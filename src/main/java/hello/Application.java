5cce93a47a09c2e1741a68d9fc3acc52d22cb8cf
Xinh Thanh Thuan <thuanxt@outlook.com>
2016-04-15 19:55:55 +0700

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by thuan_000 on 4/15/2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String [] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
